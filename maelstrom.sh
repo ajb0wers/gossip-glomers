854eb742eb15a68b042a199c1bd7763399eab764
Andre Bowers <ajbowers.plan9@gmail.com>
2025-06-10 21:38:08 +0200

    Minor formatting

--- maelstrom.sh
+++ maelstrom.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 printf "%s\n" \
 '{"src":"c1","dest":"n1","body":{"msg_id":1,"type":"init","node_id":"n1","node_ids":["n1"]}}' \
 '{"src":"c1","dest":"n1","body":{"msg_id":2,"type":"echo","echo":"hello there"}}' \
