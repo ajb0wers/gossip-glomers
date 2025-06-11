d736bc9f1cdfaa950bc00b5371ad78049e14f430
Andre Bowers <ajbowers.plan9@gmail.com>
2025-06-11 17:28:45 +0200

    Add maelstrom.sh test script

--- maelstrom.sh
+++ maelstrom.sh
@@ -5,12 +5,14 @@ cat <<EOF
 {"src":"c1","dest":"n1","body":{"msg_id":3,"type":"generate"}}
 {"src":"c1","dest":"n1","body":{"msg_id":4,"type":"topology","topology":{"n1":["n2","n3"],"n2":["n1"],"n3":["n1"]}}}
 {"src":"c1","dest":"n1","body":{"msg_id":5,"type":"broadcast","message":1}}
-{"src":"c1","dest":"n1","body":{"msg_id":6,"type":"read"}}
+{"src":"c1","dest":"n1","body":{"msg_id":6,"type":"broadcast","message":2}}
+{"src":"c1","dest":"n1","body":{"msg_id":7,"type":"broadcast","message":3}}
+{"src":"c1","dest":"n1","body":{"msg_id":8,"type":"read"}}
 EOF
 sleep 1s; cat <<EOF
-{"src":"n2","dest":"n1","body":{"in_reply_to":4,"type":"broadcast_ok"}}
+{"src":"n2","dest":"n1","body":{"in_reply_to":6,"type":"broadcast_ok"}}
 EOF
 sleep 1s; cat <<EOF
-{"src":"n3","dest":"n1","body":{"in_reply_to":5,"type":"broadcast_ok"}}
+{"src":"n3","dest":"n1","body":{"in_reply_to":7,"type":"broadcast_ok"}}
 EOF
 cat -
