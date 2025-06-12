51cbc6657a1fae9eefbc7420ed9c42281d1f82f7
Andre Bowers <ajbowers.plan9@gmail.com>
2025-06-12 17:15:21 +0200

    Minor refactor

--- maelstrom.sh
+++ maelstrom.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
 cat <<EOF
 {"src":"c1","dest":"n1","body":{"msg_id":1,"type":"init","node_id":"n1","node_ids":["n1"]}}
 {"src":"c1","dest":"n1","body":{"msg_id":2,"type":"echo","echo":"hello there"}}
