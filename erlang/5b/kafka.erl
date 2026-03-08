7189660a08b47f39d2ed7b61c0dd377dc5f4cd57
Andre Bowers <ajbowers.plan9@gmail.com>
2026-03-08 18:22:01 +0200

    kafka

---
+++ erlang/5b/kafka.erl
@@ -0,0 +1,171 @@
+#!/usr/bin/env -S escript -c
+-module(kafka).
+
+-export([rpc_request/1, rpc_reply/1]).
+
+-record(state, {
+  node_id  = null :: 'null' | binary(),
+  node_ids = []   :: [binary()],
+  data     = #{}  :: #{Key::binary() := {
+                          Length::non_neg_integer(),
+                          Commit::non_neg_integer(),
+                          Msgs :: [{Offset::non_neg_integer(), any()}]}}
+}).
+
+main([]) -> 
+  io:setopts(standard_io, [{binary, true}]),
+  loop(standard_io).
+
+loop(standard_io) -> 
+  register(rpc_request, spawn_link(?MODULE, rpc_request, [noargs])),
+  register(rpc_reply, spawn_link(?MODULE, rpc_reply, [noargs])),
+  rpc_loop().
+
+rpc_loop() ->
+  case io:get_line([]) of
+    eof -> ok;
+    {error, Reason} -> exit(Reason);
+    Line ->
+      rpc_request ! {line, Line},
+      rpc_loop()
+  end.
+
+rpc_request(noargs) ->
+  rpc_request(#state{});
+rpc_request(#state{} = State) ->
+  receive 
+    {line, Line} ->
+      {reply, Reply, NewState} = handle_line(Line, State),
+      rpc_reply ! {reply, Reply},
+      rpc_request(NewState);
+    _Other ->
+      rpc_request(State)
+  end.
+
+rpc_reply(noargs) ->
+  rpc_reply(#{});
+rpc_reply(State) ->
+  receive
+    {reply, Msg} ->
+      Reply = json:encode(Msg),
+      io:format("~s~n", [Reply]),
+      rpc_reply(State)
+  end.
+
+parse_line(Line) ->
+  Msg = json:decode(Line),
+  #{<<"src">> := Src,
+    <<"dest">> := Dest,
+    <<"body">> := Body} = Msg,
+  #{<<"type">> := Type} = Body,
+  {Type, Src, Dest, Body}.
+
+handle_line(Line, State) -> 
+  Msg = parse_line(Line),
+  handle_msg(Msg, State).
+
+handle_msg({~"init", Src, Dest, Body}, State) ->
+  #{<<"msg_id">>   := MsgId,
+    <<"node_id">>  := NodeId,
+    <<"node_ids">> := NodeIds} = Body,
+
+  NewState = State#state{
+    node_id  = NodeId,
+    node_ids = NodeIds
+  },
+
+  reply(Src, Dest, #{
+    <<"type">> => <<"init_ok">>,
+    <<"in_reply_to">> => MsgId
+  }, NewState);
+
+
+handle_msg({~"send", Src, Dest, Body}, #state{data=Streams} = State) ->
+  #{<<"key">> := K, <<"msg">> := Msg, <<"msg_id">> := MsgId} = Body,
+
+  {Offset, Logs} = append(K, Msg, Streams),
+  NewState = State#state{data=Logs},
+
+  reply(Src, Dest, #{
+    <<"type">> => <<"send_ok">>,
+    <<"offset">> => Offset,
+    <<"in_reply_to">> => MsgId
+  }, NewState);
+
+handle_msg({~"poll", Src, Dest, Body}, #state{data=Logs} = State) ->
+  #{<<"offsets">> := Offsets, <<"msg_id">> := MsgId} = Body,
+
+  Msgs = read(Offsets, Logs),
+
+  reply(Src, Dest, #{
+    <<"type">> => <<"poll_ok">>,
+    <<"msgs">> => Msgs,
+    <<"in_reply_to">> => MsgId
+  }, State);
+
+handle_msg({~"commit_offsets", Src, Dest, Body}, #state{data=Data} = State) ->
+  #{<<"offsets">> := Offsets, <<"msg_id">> := MsgId} = Body,
+
+  NewData = commit(Offsets, Data),
+  NewState = State#state{data=NewData},
+
+  reply(Src, Dest, #{
+    <<"type">> => <<"commit_offsets_ok">>,
+    <<"in_reply_to">> => MsgId
+  }, NewState);
+
+handle_msg({~"list_committed_offsets", Src, Dest, Body}, State) ->
+  #{<<"keys">> := Ks, <<"msg_id">> := MsgId} = Body,
+
+  Offsets = list(Ks, State#state.data),
+
+  reply(Src, Dest, #{
+    <<"type">> => <<"list_committed_offsets_ok">>,
+    <<"offsets">> => Offsets,
+    <<"in_reply_to">> => MsgId
+  }, State);
+
+handle_msg({_Tag, _Src, _Dest}, State) -> {ok, State}.
+
+reply(Dest, Src, Body, State) when State#state.node_id =:= Src ->
+  reply(Dest, Body, State).
+
+reply(Dest, Body, State) -> 
+  Reply = #{
+    <<"dest">> => Dest, 
+    <<"src">>  => State#state.node_id,
+    <<"body">> => Body},
+  {reply, Reply, State}.
+
+append(Key, Msg, Logs) ->
+  CreateIfNotExists = {0,0,[]},
+  {Length, Commit, List} = maps:get(Key, Logs, CreateIfNotExists),
+  Offset = Length+1,
+  Appended = Logs#{Key => {Offset, Commit, [{Offset, Msg}]++List}}, 
+  {Offset, Appended}.
+
+read(Offsets, Logs) ->
+  maps:fold(fun (K, From, AccIn) ->
+    case Logs of 
+      #{K := {_Length, _Commit, Log}} ->
+        %% Pred = fun({I,_}) -> I >= From andalso I > Commit end,
+        Pred = fun({I,_}) -> I >= From end,
+        List = lists:takewhile(Pred, Log),
+        Queue = lists:reverse([[I,H] || {I,H} <- List]),
+        AccIn#{K => Queue};
+      _NoMatch -> AccIn
+    end
+  end, #{}, Offsets).
+
+commit(Offsets, Logs) ->
+  maps:fold(fun (K, End, AccIn) -> 
+    case Logs of
+      #{K := {Offset, Commit, Log}} when End > Commit ->
+        AccIn#{K := {Offset, End, Log}};
+      _ -> AccIn
+    end
+  end, Logs, Offsets).
+
+list(Keys, Logs) ->
+  Map = maps:with(Keys, Logs),
+  #{K => Commit || K := {_,Commit,_} <- Map}.
