7b625c8e70865c7f087001ff39bb4f2d7debd843
Andre Bowers <ajbowers.plan9@gmail.com>
2026-07-12 12:48:48 +0200

    kafka-5b

--- erlang/5b/debug.sh
+++ erlang/5b/debug.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 kafka() {
-  printf "%s\n" '{"src":"c1","dest":"n1","body":{"msg_id":1,"type":"init","node_id":"n1","node_ids":["n1","n2","n3"]}}'
+  printf "%s\n" '{"src":"c1","dest":"n1","body":{"msg_id":1,"type":"init","node_id":"n1","node_ids":["n0","n1"]}}'
   # {"body":{"in_reply_to":1,"type":"init_ok"},"dest":"c1","src":"n1"}
   printf "%s\n" '{"src":"c1","dest":"n1","body":{"msg_id":2,"type":"send","key":"k1","msg":9}}'
   # {"body":{"key":"k1","msg_id":1,"type":"read"},"dest":"lin-kv","src":"n1"}
