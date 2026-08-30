1277274bc9301042cef05d386529ce108f82d2c1
Andre Bowers <ajbowers.plan9@gmail.com>
2026-08-30 21:14:15 +0200

    txn-6b

--- erlang/6b/txn.erl
+++ erlang/6b/txn.erl
@@ -172,3 +172,11 @@ parse_line(Line) ->
   #{<<"type">> := Type} = Body,
   {Type, Src, Dest, Body}.
 
+-doc """
+https://antonz.org/uuidv7/#erlang
+""".
+-spec generate() -> binary().
+generate() ->
+    <<RandA:12, RandB:62, _:6>> = crypto:strong_rand_bytes(10),
+    UnixTsMs = os:system_time(millisecond), Ver = 2#0111, Var = 2#10,
+    <<UnixTsMs:48, Ver:4, RandA:12, Var:2, RandB:62>>.
